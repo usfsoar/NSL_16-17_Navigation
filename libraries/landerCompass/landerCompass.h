e4d88605ac9d805fcbbbcd8383da8c03c883b57f
elfyyy <kyleh04@ymail.com>
2017-02-18 14:56:43 -0500

    Adds landerCompass

---
+++ libraries/landerCompass/landerCompass.h
@@ -0,0 +1,23 @@
+#include "Arduino.h"
+#include <Adafruit_LSM303_U.h>
+#include <Adafruit_BMP085_U.h>
+#include <Adafruit_L3GD20_U.h>
+#include <Adafruit_10DOF.h>
+
+class landerCompass {
+	
+	public:
+		struct latLon {
+			float north, west;
+		};
+		float getNeededHeading(latLon currLoc, latLon neededLoc), getCurrentHeading();
+		void initDofBoard();
+
+	
+	private:
+		Adafruit_LSM303_Accel_Unified accel;
+		Adafruit_10DOF dof;
+		Adafruit_LSM303_Mag_Unified mag;
+		Adafruit_BMP085_Unified bmp;
+		Adafruit_L3GD20_Unified gyro;
+};
