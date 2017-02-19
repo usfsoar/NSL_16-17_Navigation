7d21d416130783e41c24713f998e1c291a92222e
elfyyy <kyleh04@ymail.com>
2017-02-19 15:52:42 -0500

    Removes non-needed code from main file

--- SOAR_NSL_Navigation.ino
+++ SOAR_NSL_Navigation.ino
@@ -1,88 +1,29 @@
-#include <Timer.h>
-#include <landerGPS.h>
-#include <landerCompass.h>
-#include <Servo.h>
-
-int startMotorValue = 1250;
-
-int leftMotorPin = 6;
-int rightMotorPin = 7;
-Servo leftMotor;
-Servo rightMotor;
-
-boolean rightMotorOn = false;
-boolean leftMotorOn = false;
-
-uint32_t startTime, timer1;
+#include "Timer.h"
+#include "Lander.h"
 
 struct latLon {
-  public:
    float north, west;   
 };  
 
-void turnLeftMotorOn(int val) {
-  leftMotor.writeMicroseconds(val);
-}
-
-void turnLeftMotorOff() {
-  leftMotor.writeMicroseconds(1100);
-}
-
-void turnRightMotorOn(int val) {
-  rightMotor.writeMicroseconds(val);
-}
-
-void turnRightMotorOff() {
-  rightMotor.writeMicroseconds(1100);
-}
-
-void initMotors() {
-  Serial.println("Initializing Motors");
-  leftMotor.attach(leftMotorPin);
-  rightMotor.attach(rightMotorPin);
-  turnRightMotorOff();
-  turnLeftMotorOff();
-  delay(10000);  
-  Serial.println("Motors Initialized");
-  turnRightMotorOn(startMotorValue);
-  turnLeftMotorOn(startMotorValue);
-}
-
-void failSafeTimer(int timeToShutOffMS) {
-  if (millis() - timer1 > timeToShutOffMS) {
-    turnRightMotorOff();
-    turnLeftMotorOff();
-    delay(100000);
-  }
-}
-
-//void runMotors(int start, int reductionFactor, int heading){
-//  int difference = getCurrentHeading() - heading;
-//  
-//  if (difference < 0) {
-//    difference += 360;
-//  }
-//
-//  if (difference <= 180) {
-//    turnRightMotorOn(start + (difference / reductionFactor));
-//    turnLeftMotorOn(start);
-//  } else {
-//    turnLeftMotorOn(start + ((360 - difference) / reductionFactor));
-//    turnRightMotorOn(start);
-//  }  
-//}
-
 latLon neededLatLon;
+Lander lander;
+Timer failTimer;
 
 void setup()  {
   Serial.begin(115200);
-  initMotors();
-  neededLatLon.north = 28.048582; 
-  neededLatLon.west = -82.410679;
+  lander.motors.setLeftPin(6);
+  lander.motors.setRightPin(7);
+  lander.motors.setMinVal(1250);
+  lander.compass.enable(true);
+  lander.gps.enable(false);
+  lander.motors.enable(true);
+  lander.motors.setMinVal(1250);
+  failTimer.start();
 }
 
 void loop(){  
-  failSafeTimer(20000);
-  delay(20);
-  //runMotors(startMotorValue, 4, 90);
+  while (failTimer.getElapsedTime() < 10 * 1000) {
+    delay(20);
+    lander.navigateTo(4, 90);
+  }
 }
