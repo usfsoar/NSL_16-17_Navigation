4068252353e1e7695369541b539548e4b02d0edf
Kyle <kylehunter@mail.usf.edu>
2017-04-05 11:55:28 -0400

    Adds lattitude and longitude sending to Pi

--- libraries/comms/comms.h
+++ libraries/comms/comms.h
@@ -4,4 +4,5 @@ extern bool initComms();
 extern bool piInit(), needToShutdown();
 
 
-extern void setCommAltitude(int alt), setCommDistance(int dist), setDofError(bool er), setGpsError(bool er), setgpsHasFix(bool er);
+extern void setCommAltitude(int alt), setCommDistance(int dist), setDofError(bool er), setGpsError(bool er), setGpsHasFix(bool er);
+extern void setCommLat(float lat), setComLon(float lon);
